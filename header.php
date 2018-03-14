d00214c6c0c0d99309ac2d1f4fbc0a17cec7955c
Polkovnik0ff <polkovnikov.andrew@gmail.com>
2018-03-15 01:30:13 +0700

    add icon

--- header.php
+++ header.php
@@ -1,67 +1,54 @@
 <!doctype html>
 <html lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="img/favicon.ico" type="image/x-icon">
-        <title>ИС "Сервера Moodle"</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <title>ИС "Сервера Moodle"</title>
 
-        <!-- Latest compiled and minified CSS -->
-        <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <style type="text/css">
-            body {
-                padding-top: 70px;
-                padding-bottom: 30px;
-            }
-
-            .theme-dropdown .dropdown-menu {
-                position: static;
-                display: block;
-                margin-bottom: 20px;
-            }
-
-            .theme-showcase > p > .btn {
-                margin: 5px 0;
-            }
-
-            .theme-showcase .navbar .container {
-                width: auto;
-            }
-        </style>
-        <link rel="stylesheet" href="css/styles.css"
-    </head>
-    <body>
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">ИС Сервера Moodle</a>
-                </div>
-                <div id="navbar" class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav navbar-left">
-                        <li><a href="http://kemsu.ru/">КемГУ</a> </li>
-                        <li><a href="http://iais.kemsu.ru/">Иаис</a> </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-3 col-md-2 sidebar">
-                    <ul class="nav nav-sidebar">
-                        <li><a href="#">Регистрация Сервера</a></li>
-                        <li><a href="#">Отчёты</a></li>
-                        <li><a href="#">Связка пользователей</a></li>
-                        <li><a href="#">Связка дисцеплин</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+    <!-- Latest compiled and minified CSS -->
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <style type="text/css">
+        body {
+            padding-top: 70px;
+            padding-bottom: 30px;
+        }
+    </style>
+    <link rel="stylesheet" href="css/styles.css"
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-education"></span> ИС Сервера Moodle</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-left">
+                <li><a href="http://kemsu.ru/">КемГУ</a></li>
+                <li><a href="http://iais.kemsu.ru/">Иаис</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+            <ul class="nav nav-sidebar">
+                <li><a href="#"><span class="glyphicon glyphicon-tasks"></span>Регистрация Сервера</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-calendar"></span>Отчёты</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span>Связка пользователей</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-link"></span>Связка дисцеплин</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
